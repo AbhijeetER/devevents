f63ede1b728342dc2227fa0677667f61669f2f90
AbhijeetER <206299249+AbhijeetER@users.noreply.github.com>
2026-04-19 23:27:06 +0530

    added database models as per next

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ExploreBtn from './components/ExploreBtn'
-
+import { title } from 'process'
+import EventCard from '@/components/EventCard'
+import { events } from '@/lib/Constants'
 const page = () => {
   return (
     <section>
-      <h1 className='text-center '>The Hub for every Dev <br/> Event You Can't Miss</h1>
+      <h1 className='text-center py-8 '>The Hub for every Dev <br/> Event You Can't Miss</h1>
       <p className='text-center mt-5'>hackathons, meetups and conferences all in one place</p>
       <ExploreBtn/>
 
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
         <ul className='events'>
-          {[1,2,3,4,5].map((e)=>(
-            <li key={e}>Event {e}</li>
+          {events.map((e)=>(
+            <li key={e.title}>
+              <EventCard {...e}/>
+            </li>
           ))}
         </ul>
       </div>
